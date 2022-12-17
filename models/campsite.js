a370f0abca480f123e36ab5dff048bead516b991
Josue Iniguez <josue.i@forever21.com>
2022-12-16 19:27:57 -0800

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+// Create Comments Schemas
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+// Create Campsites Schemas
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema] // Comments subdocument
+}, {
+    timestamps: true
+});
+
+// Create Campsite Model
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
